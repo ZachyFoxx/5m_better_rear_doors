32ce9ed6b4a449754e0d9efb194c86c2c75a30d9
Zachery Elliot <zachery@foxboy.sh>
2021-09-23 13:28:57 -0500

    Implement /rear command
    bug: A vehicle is never detected when you execute /rear

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
 fx_version 'bodacious'
 game 'gta5'
 
-name 'FiveM Typescript Boilerplate'
-description 'A boilerplate for using Typescript in FiveM'
-author 'Remco Troost (d0p3t)'
-url 'https://github.com/d0p3t/fivem-ts-boilerplate'
+name 'Better Rear Doors'
+description 'A better way to open the rear doors of vehicles'
+author 'ZachyFoxx'
+url 'https://github.com/ZachyFoxx/5m_better_rear_doors'
 
 client_script 'dist/client/*.client.js'
 server_script 'dist/server/*.server.js'
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,27 @@
-setImmediate(() => {
-  emitNet('helloserver');
-});
+RegisterCommand("rear", function() {
+  TriggerEvent("zf:toggleRearDoors")
+}, false);
+TriggerEvent("zf:toggleRearDoors", "/rear", "")
 
-onNet('helloclient', message => {
-  console.log(`The server replied: ${message}`);
-});
+AddEventHandler("zf:toggleRearDoors", function() {
+  const [hit, ent, coords, norm, dist] = vehicleInFront();
+  if (hit && IsEntityAVehicle(ent)) {
+    const vehicle = ent;
+    if (IsVehicleDoorFullyOpen(vehicle, 6) && IsVehicleDoorFullyOpen(vehicle, 7)) {
+      SetVehicleDoorShut(vehicle, 6, false);
+      SetVehicleDoorShut(vehicle, 7, false);
+    } else {
+      SetVehicleDoorOpen(vehicle, 6, false, false);
+      SetVehicleDoorOpen(vehicle, 7, false, false);
+    }
+  }
+})
+
+function vehicleInFront(){
+  const player = PlayerPedId();
+  const pos = GetEntityCoords(player, true);
+  const entWorld = GetOffsetFromEntityInWorldCoords(player, 0.0, 20.0, 0.0);
+  const ray = CastRayPointToPoint(pos[0], pos[1], pos[2], entWorld[0], entWorld[1], entWorld[2], 2, player, 0);
+  const hit = GetRaycastResult(ray);
+  return hit;
+}
